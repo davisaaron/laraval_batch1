dea3ae2d1c250d9078a02cdec3a3fb845e069e34
mydynamica01 <xboyeye@gmail.com>
2021-11-10 17:46:19 +0530

    chech1

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::resource('employees',EmployeeController::class);
+Route::resource('employees',EmployeeController::class);
