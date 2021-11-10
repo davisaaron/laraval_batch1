7a3d0059bedb475e301f5145aa175b9071b7544c
mydynamica01 <mydynamica01@gmail.com>
2021-11-10 17:20:14 +0530

    first commit

---
+++ resources/views/employee/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<form action="{{route('employees.store')}}" method=post>
+    @csrf
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname" value=""><br>
+  <label for="dob">DOB</label>
+  <input type="text" id="dob" name="dob" value=""><br>
+  <label for="salary">Salary</label>
+  <input type="salary" id="salary" name="salary" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+
+</body>
+</html>
